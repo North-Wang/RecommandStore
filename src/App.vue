d0b98f1e22f302b26b6829008cae94a90aa22683
North-Wang <103781180+North-Wang@users.noreply.github.com>
2023-11-21 17:37:58 +0800

    install router

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,6 @@
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <div>測試部屬</div>
-    <div>測試部屬</div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
