997b3d5b9727b2a106f77a41cc27b9081ac9d290
North-Wang <b10234033.ntust@gmail.com>
2023-11-27 07:45:17 +0800

    Homepage.vue

--- src/component/Homepage.vue
+++ src/component/Homepage.vue
@@ -10,15 +10,16 @@ import { ref, onMounted, watch, computed } from "vue";
 import axios from "axios";
 
 const addNewStore = async function () {
-  const data = ["John Doe", 30, "Coding"];
-  axios
-    .post(
-      "https://sheets.googleapis.com/v4/spreadsheets/1_3W1EeAV2n4InaUCczUp7BVPzXxB1mVsneRrD7Ygb_0/values/工作表1?key=AIzaSyD4tjE_hNQpGPegRSGPD-Ut_Avo9G59zgU",
-      data
-    )
-    .then((result) => {
-      console.log("新增資料", result);
-    });
+  console.log("SpreadsheetApp", SpreadsheetApp);
+  // const data = ["John Doe", 30, "Coding"];
+  // axios
+  //   .post(
+  //     "https://sheets.googleapis.com/v4/spreadsheets/1_3W1EeAV2n4InaUCczUp7BVPzXxB1mVsneRrD7Ygb_0/values/工作表1?key=AIzaSyD4tjE_hNQpGPegRSGPD-Ut_Avo9G59zgU",
+  //     data
+  //   )
+  //   .then((result) => {
+  //     console.log("新增資料", result);
+  //   });
 };
 </script>
 <style scoped></style>
