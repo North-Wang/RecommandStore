9cf29d99a7a7e347ba3378734241bca011aedc2f
North-Wang <103781180+North-Wang@users.noreply.github.com>
2023-11-27 13:42:24 +0800

    測試新增店家資料

--- src/component/Homepage.vue
+++ src/component/Homepage.vue
@@ -10,16 +10,34 @@ import { ref, onMounted, watch, computed } from "vue";
 import axios from "axios";
 
 const addNewStore = async function () {
-  console.log("SpreadsheetApp", SpreadsheetApp);
-  // const data = ["John Doe", 30, "Coding"];
-  // axios
-  //   .post(
-  //     "https://sheets.googleapis.com/v4/spreadsheets/1_3W1EeAV2n4InaUCczUp7BVPzXxB1mVsneRrD7Ygb_0/values/工作表1?key=AIzaSyD4tjE_hNQpGPegRSGPD-Ut_Avo9G59zgU",
-  //     data
-  //   )
+  const newStore = {
+    name: "北大荒水餃店",
+    type: "日式",
+    peopleLimit: "10",
+    purple: "普通吃飯",
+    lowPrice: "60",
+    highPrice: "300",
+    address: "115台北市南港區南港路一段201號",
+    feature: "划算",
+    category: "滷味,水餃",
+  };
+  const config = {
+    method: "Post",
+    body: newStore,
+  };
+  const url =
+    "https://script.google.com/macros/library/d/1vFlvViGJaZRYNHJygvz9ImyQsZV38dszpYS6kgH1i_g-EMHSn4-S69OX/1";
+  // fetch(url, config)
+  //   .then((response) => response.json())
   //   .then((result) => {
-  //     console.log("新增資料", result);
-  //   });
+  //     if (result === "success") {
+  //       alert("資料已成功送出，我們下次見~");
+  //     }
+  //   })
+  //   .catch((error) => console.log("error", error));
+  axios.post(url, newStore).then((result) => {
+    console.log("result", result);
+  });
 };
 </script>
 <style scoped></style>
