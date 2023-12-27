5621e101213257f24fe454a7a572747bcbb35e6a
North-Wang <103781180+North-Wang@users.noreply.github.com>
2023-12-27 11:24:46 +0800

    custom Tailwind.cdd screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -24,3 +24,28 @@ export default {
   },
   plugins: [require("tailwindcss")],
 };
+// export default {
+//   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       screens: {
+//         //要超過此寬度才會套用
+//         sm: "640px",
+//         // => @media (min-width: 640px) { ... }
+
+//         md: "768px",
+//         // => @media (min-width: 768px) { ... }
+
+//         lg: "1024px",
+//         // => @media (min-width: 1024px) { ... }
+
+//         xl: "1280px",
+//         // => @media (min-width: 1280px) { ... }
+
+//         "2xl": "1536px",
+//         // => @media (min-width: 1536px) { ... }
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss")],
+// };
