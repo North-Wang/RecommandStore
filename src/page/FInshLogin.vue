5234cef4bd67032ee5d7aa8c0baad2b6c7842b25
North-Wang <103781180+North-Wang@users.noreply.github.com>
2025-01-10 16:19:50 +0800

    <test>line login

---
+++ src/page/FInshLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="p-4 grid grid-cols-2 grid-rows-2">
+    <div>完成登入</div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import axios from "axios";
+
+const getToken = async () => {
+  try {
+    const response = await axios.get(
+      "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1234567890&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%3Fkey%3Dvalue&state=12345abcde&scope=profile%20openid&nonce=09876xyz"
+    );
+    const token = response.data.access_token;
+    console.log("Token:", token);
+  } catch (error) {
+    console.error("Error fetching token:", error);
+  }
+};
+onMounted(() => {
+  getToken();
+});
+</script>
--- src/page/TestLogin.vue
+++ src/page/TestLogin.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-4 grid grid-cols-2 grid-rows-2">
-    <div>測試Line登入</div>
     <a :href="lineLoginUrl">Line 登入</a>
   </div>
 </template>
@@ -10,8 +9,10 @@ import { ref, computed, onMounted } from "vue";
 import axios from "axios";
 
 const lineLoginUrl = computed(() => {
-  const channelId = "2006738100";
+  const channelId = 2006738100;
   const callbackUrl = "https://test-versel-rho.vercel.app/LineCallBackUrl.php";
+  const generateState = () => Math.random().toString(36).substring(2, 15);
+  const state = generateState();
   const timeStamp = new Date().getTime();
   return `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${channelId}&redirect_uri=${callbackUrl}&state=${timeStamp}&scope=profile%20openid&nonce=09876xyz`;
 });
